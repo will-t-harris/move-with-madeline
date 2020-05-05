efac8ed9df18a743b1e587de414d165f9e4765e3
Will Harris <will.t.harris@protonmail.com>
2020-05-04 20:20:34 -0700

    test adding editor components

---
+++ src/cms/cms.js
@@ -0,0 +1,6 @@
+import React from "react"
+import CMS from "netlify-cms-app"
+
+import ContentBlock from "../components/ContentBlock"
+
+CMS.registerEditorComponent(ContentBlock)
---
+++ src/components/ContentBlock.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const ContentBlock = () => {
+  return <div>Test</div>
+}
+
+export default ContentBlock
