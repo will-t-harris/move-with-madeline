19512552b1db458823af2f6b6e5bdb3047699582
Will Harris <will.t.harris@protonmail.com>
2020-05-04 20:31:40 -0700

    attempt at registering editor component

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,4 +3,17 @@ import CMS from "netlify-cms-app"
 
 import ContentBlock from "../components/ContentBlock"
 
-CMS.registerEditorComponent("Content Block", ContentBlock)
+CMS.registerEditorComponent({
+  id: "content-block",
+  label: "Content Block",
+  fields: [
+    { name: "post-content", label: "Post Content Block", widget: "text" },
+  ],
+  pattern: /^<ContentBlock/,
+  fromBlock: match => {
+    return {
+      id: match[1],
+    }
+  },
+  toBlock: obj => "content " + obj.id,
+})
