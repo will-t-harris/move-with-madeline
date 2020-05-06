2d611c1a17368769b2e5b1339d7fb02f2a55dfd6
Will Harris <will.t.harris@protonmail.com>
2020-05-06 09:26:49 -0700

    add intro section component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
+import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
