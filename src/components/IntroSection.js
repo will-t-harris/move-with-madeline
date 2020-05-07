4cad28823112c012dfd5a760fa0769ba4b6ac8b3
Will Harris <will.t.harris@protonmail.com>
2020-05-06 17:12:28 -0700

    adjust intro section spacing and line-height

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -25,17 +25,17 @@ const IntroSection = () => {
   return (
     <div>
       <div className="flex flex-col">
-        <h1 className="text-5xl text-center font-subheader leading-tight py-16">
+        <h1 className="text-5xl text-center font-subheader font-medium leading-tight p-16">
           {data.file.childMdx.frontmatter.title}
         </h1>
-        <p className="font-content text-center mx-4 leading-5 pb-16">
+        <p className="font-content mx-4 leading-relaxed opacity-75 pb-16 px-4">
           {data.file.childMdx.frontmatter.intro}
         </p>
       </div>
-      <div className="mx-4">
+      <div>
         <Img
           fluid={data.imageSharp.fluid}
-          className="h-352 rounded-tl-xl rounded-br-xl"
+          className="h-352 rounded-tl-xl rounded-br-xl mx-8"
         />
       </div>
     </div>
