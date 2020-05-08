7ced3c0a35803a8aa41b90a992586680bbde7e87
Will Harris <will.t.harris@protonmail.com>
2020-05-08 11:52:35 -0700

    add social links and email to foote

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,26 @@ import AmazonIcon from "./icons/AmazonIcon"
 
 const Footer = () => {
   return (
-    <div className="flex flex-col h-40 bg-bgPrimary">
+    <footer className="flex flex-col h-40 bg-bgPrimary">
       <div className="mx-auto pt-16 pb-8 flex flex-row h-20">
-        <YoutubeIcon twClasses="mr-2" />
-        <InstagramIcon twClasses="mr-2" />
+        <a href="https://www.youtube.com/channel/UCPYe2LOAGOJOesjCOBMd_Sg/">
+          <YoutubeIcon twClasses="mr-2" />
+        </a>
+        <a href="https://www.instagram.com/move.with.madeline/">
+          <InstagramIcon twClasses="mr-2" />
+        </a>
         <TwitterIcon twClasses="mr-2" />
         <AmazonIcon />
       </div>
       <div className="mx-auto opacity-50">
         All content &copy; Madeline Walkon
       </div>
-    </div>
+      <div className="mx-auto opacity-50">
+        <a href="mailto:hello@movewithmadeline.com">
+          hello@movewithmadeline.com
+        </a>
+      </div>
+    </footer>
   )
 }
 
