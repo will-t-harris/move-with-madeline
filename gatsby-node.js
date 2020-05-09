2751cf9805a2407a60a1d6cf06bc189ffbcec3e6
Will Harris <will.t.harris@protonmail.com>
2020-05-09 15:25:38 -0700

    change slug in createNodeField

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   if (node.internal.type === "MarkdownRemark") {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
 
-    createNodeField({ node, name: `slug`, value: `/fitness${slug}` })
+    createNodeField({ node, name: `slug`, value: `${slug}` })
   }
 }
 
