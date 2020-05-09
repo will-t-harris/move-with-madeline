cd3e49d539bbd5562dc3a037ddd79ff1fa057b5a
Will Harris <will.t.harris@protonmail.com>
2020-05-09 15:26:04 -0700

    add fitness index page

--- src/pages/fitness.js
+++ src/pages/fitness.js
@@ -1,10 +1,34 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const FitnessPage = () => {
+  const data = useStaticQuery(graphql`
+    query fitnessPageQuery {
+      markdownRemark(fields: { slug: { eq: "/fitness/" } }) {
+        frontmatter {
+          title
+        }
+      }
+      imageSharp(fluid: { originalName: { eq: "fitness-01.png" } }) {
+        id
+        fluid(quality: 90, maxWidth: 1920) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  `)
+
   return (
     <>
-      <div>Sliced image</div>
-      <div>Card</div>
+      <div>
+        <Img fluid={data.imageSharp.fluid} className="index-image-clip-path" />
+      </div>
+      <div>
+        <h1 className="text-center text-2xl font-content">
+          {data.markdownRemark.frontmatter.title.toUpperCase()}
+        </h1>
+      </div>
     </>
   )
 }
