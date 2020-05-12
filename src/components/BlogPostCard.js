657dd03989b461f64ed36c98e854fe2cc76800d0
Will Harris <will.t.harris@protonmail.com>
2020-05-12 16:01:31 -0700

    remove unused imports

--- src/components/BlogPostCard.js
+++ src/components/BlogPostCard.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql, Link } from "gatsby"
-import Img from "gatsby-image"
+import { Link } from "gatsby"
 
 const BlogPostCard = ({ data }) => {
   return (
