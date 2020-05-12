1cc75bd801a0f9fdb54c2a372bc104eaae0014ec
Will Harris <will.t.harris@protonmail.com>
2020-05-12 10:30:15 -0700

    createPages for fitness post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // Add slug nodes to graph on build
@@ -11,8 +12,45 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-// exports.createPages = async ({ graphql, actions, reporter }) => {
-//   const { createPage } = actions
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const FitnessPostTemplate = path.resolve(
+    "src/templates/fitnessPostTemplate.js"
+  )
 
-//   const result = await graphql(``)
-// }
+  const result = await graphql(`
+    {
+      fitnessPosts: allFile(
+        filter: { sourceInstanceName: { eq: "fitness-posts" } }
+      ) {
+        nodes {
+          childMarkdownRemark {
+            fields {
+              slug
+            }
+            frontmatter {
+              topImage
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild("Error while running GraphQL query")
+    return
+  }
+
+  result.data.fitnessPosts.nodes.forEach((node) => {
+    const { childMarkdownRemark } = node
+    createPage({
+      path: `/fitness${childMarkdownRemark.fields.slug}`,
+      component: FitnessPostTemplate,
+      context: {
+        slug: childMarkdownRemark.fields.slug,
+        topImage: childMarkdownRemark.frontmatter.topImage,
+      },
+    })
+  })
+}
