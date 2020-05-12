9099af21a0b89753508e9918a41a0c19a0a275a5
Will Harris <will.t.harris@protonmail.com>
2020-05-12 16:59:40 -0700

    fix intro section query

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -5,10 +5,12 @@ import Img from "gatsby-image"
 const IntroSection = () => {
   const data = useStaticQuery(graphql`
     query indexPageQuery {
-      markdownRemark(id: { eq: "ea78fdb9-afaa-5282-b440-d2a41485b4d7" }) {
-        frontmatter {
-          title
-          intro
+      file(sourceInstanceName: { eq: "index-pages" }, name: { eq: "index" }) {
+        childMarkdownRemark {
+          frontmatter {
+            title
+            intro
+          }
         }
       }
       imageSharp(fluid: { originalName: { eq: "madeline-01.jpg" } }) {
@@ -23,10 +25,10 @@ const IntroSection = () => {
     <div className="lg:flex">
       <div className="flex flex-col">
         <h1 className="text-5xl text-center font-subheader font-medium leading-tight p-16 lg:mt-32">
-          {data && data.markdownRemark.frontmatter.title}
+          {data.file.childMarkdownRemark.frontmatter.title}
         </h1>
         <p className="font-content mx-4 leading-relaxed opacity-75 pb-16 px-4 lg:w-1/2 lg:mx-auto lg:mb-32 lg:p-0">
-          {data.markdownRemark.frontmatter.intro}
+          {data.file.childMarkdownRemark.frontmatter.intro}
         </p>
       </div>
       <div className="lg:w-1/2 lg:flex-end lg:mr-32 lg:my-32">
