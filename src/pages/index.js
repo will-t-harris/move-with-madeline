1f00135be2e77c089a46b32e520a9a42524c0cf0
Will Harris <will.t.harris@protonmail.com>
2020-05-12 11:54:14 -0700

    remove swoop and background color

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,21 +23,10 @@ const IndexPage = () => {
     <>
       <SEO title="Home" />
       <Img fluid={data.file.childImageSharp.fluid} className="h-552" />
-      <div className="bg-bgPrimary">
+      <div>
         <IntroSection />
       </div>
-      <div style={{ height: "150px", overflow: "hidden" }}>
-        <svg
-          viewBox="0 0 500 150"
-          preserveAspectRatio="none"
-          style={{ height: "100%", width: "100%" }}
-        >
-          <path
-            d="M-29.91 95.02c124.15 156.91 366.25-231.9 532.16 15.8L500 0H0z"
-            className="fill-current text-bgPrimary"
-          />
-        </svg>
-      </div>
+
       <InstagramBanner />
     </>
   )
