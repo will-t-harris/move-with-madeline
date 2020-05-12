43446075c92c86df2de249eaf94cc2d0b69f1395
Will Harris <will.t.harris@protonmail.com>
2020-05-12 11:49:08 -0700

    work on blog post card

--- src/components/BlogPostCard.js
+++ src/components/BlogPostCard.js
@@ -5,12 +5,21 @@ import Img from "gatsby-image"
 
 const BlogPostCard = ({ data }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      Image Goes Here
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{data.frontmatter.title}</div>
-        <p className="text-gray-700 text-base">{data.frontmatter.cardText}</p>
-        <Link to={`/fitness/${data.fields.slug}`}>Link to post</Link>
+    <div className="mx-auto w-352 rounded-lg shadow-lg">
+      <img src={data.frontmatter.topImage} className="rounded-lg" />
+      <div className="flex flex-col px-6 py-4 bg-gray-200 rounded-b-lg">
+        <h2 className="mb-2 font-bold text-textDark text-xl text-center font-content">
+          {data.frontmatter.title}
+        </h2>
+        <p className="text-textDark text-base opacity-75 mb-4">
+          {data.frontmatter.cardText}
+        </p>
+        <Link
+          to={`/fitness/${data.fields.slug}`}
+          className="font-content text-center bg-textDark text-bgPrimary py-2 rounded-lg"
+        >
+          READ MORE
+        </Link>
       </div>
     </div>
   )
