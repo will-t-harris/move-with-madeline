1c6a6dca128a3e1ef284445a52dc1db76e577a38
Will Harris <will.t.harris@protonmail.com>
2020-05-12 16:00:54 -0700

    tweak intro section spacing

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -22,14 +22,14 @@ const IntroSection = () => {
   return (
     <div className="lg:flex">
       <div className="flex flex-col">
-        <h1 className="text-5xl text-center font-subheader font-medium leading-tight p-16 lg:mt-40">
+        <h1 className="text-5xl text-center font-subheader font-medium leading-tight p-16 lg:mt-32">
           {data.markdownRemark.frontmatter.title}
         </h1>
-        <p className="font-content mx-4 leading-relaxed opacity-75 pb-16 px-4 lg:w-1/2 lg:mx-auto lg:mb-40">
+        <p className="font-content mx-4 leading-relaxed opacity-75 pb-16 px-4 lg:w-1/2 lg:mx-auto lg:mb-32 lg:p-0">
           {data.markdownRemark.frontmatter.intro}
         </p>
       </div>
-      <div className="lg:w-1/2 lg:flex-end lg:mr-32 lg:my-40">
+      <div className="lg:w-1/2 lg:flex-end lg:mr-32 lg:my-32">
         <Img
           fluid={data.imageSharp.fluid}
           className="h-352 rounded-tl-xl rounded-br-xl mx-8"
