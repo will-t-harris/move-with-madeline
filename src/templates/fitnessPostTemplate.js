106ac895aa43dc1f8d702ece9e526ffc9f30979c
Will Harris <will.t.harris@protonmail.com>
2020-05-18 14:21:37 -0700

    add markdown class to templates

--- src/templates/fitnessPostTemplate.js
+++ src/templates/fitnessPostTemplate.js
@@ -8,14 +8,15 @@ const FitnessPostTemplate = ({ data }) => {
 
   return (
     <div>
-      <img src={post.frontmatter.topImage} className="w-full" />
-      <div>
-        <h1 className="text-center text-2xl font-content">
-          {post.frontmatter.title}
-        </h1>
+      <img
+        src={post.frontmatter.topImage}
+        className="w-full h-640 object-cover object-top"
+      />
+      <div className="markdown">
+        <h1 className="text-center">{post.frontmatter.title}</h1>
         <div
           dangerouslySetInnerHTML={{ __html: post.html }}
-          className="mx-6"
+          className="markdown"
         ></div>
       </div>
     </div>
