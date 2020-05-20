a55afbee85c41b1d65d5fa8531507913b7b04791
Will Harris <will.t.harris@protonmail.com>
2020-05-20 10:27:07 -0700

    refactor: remove two icons, add amazon link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,12 @@ const Footer = () => {
   return (
     <footer className="flex flex-col h-24">
       <div className="mx-auto pt-4 pb-4 flex flex-row h-20">
-        <a href="https://www.youtube.com/channel/UCPYe2LOAGOJOesjCOBMd_Sg/">
-          <YoutubeIcon twClasses="mr-2 text-textDark opacity-75" />
-        </a>
         <a href="https://www.instagram.com/move.with.madeline/">
           <InstagramIcon twClasses="mr-2 text-textDark opacity-75" />
         </a>
-        <TwitterIcon twClasses="mr-2 text-textDark opacity-75" />
-        <AmazonIcon twClasses=" text-textDark opacity-75" />
+        <a href="https://www.amazon.com/shop/influencer-23600ba5?ref=cm_sw_em_r_inf_own_influencer-23600ba5_dp_DRKuGImMtufAN">
+          <AmazonIcon twClasses=" text-textDark opacity-75" />
+        </a>
       </div>
       <div className="mx-auto text-textDark opacity-75 cursor-default">
         All content &copy; Madeline Walkon
