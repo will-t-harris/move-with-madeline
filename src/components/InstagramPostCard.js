430d8b11b78f24fe5d26c9f3be2bd9493bb3a4a8
Will Harris <will.t.harris@protonmail.com>
2020-06-03 18:00:14 -0700

    fix: remove instagram banner for now

--- src/components/InstagramPostCard.js
+++ src/components/InstagramPostCard.js
@@ -1,8 +1,8 @@
-import React from "react"
-import Img from "gatsby-image"
+// import React from "react"
+// import Img from "gatsby-image"
 
-const InstagramPostCard = ({ cardImgSrc }) => {
-  return <Img fixed={cardImgSrc} className="shadow-lg" />
-}
+// const InstagramPostCard = ({ cardImgSrc }) => {
+//   return <Img fixed={cardImgSrc} className="shadow-lg" />
+// }
 
-export default InstagramPostCard
+// export default InstagramPostCard
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Img from "gatsby-image"
 
 import SEO from "../components/SEO"
 import IntroSection from "../components/IntroSection"
-import InstagramBanner from "../components/InstagramBanner"
+// import InstagramBanner from "../components/InstagramBanner"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
